0d73e2a9a805137ab2955d416ada7e472cb9ad95
Alberto Cortés Montalvo <albertocortes@reforestamos.org>
2026-03-13 14:38:12 -0600

    change images route

--- src/imgs/index.js
+++ src/imgs/index.js
@@ -1,33 +1,33 @@
 // Backgrounds 
-export { default as backgroundPtincipal }          from '../imgs/bacgroundPrincipal.png';
-export { default as backgroundPtincipalVerde }     from '../imgs/background-principal-verde.png';
-export { default as backgroundVallartaHero }       from '../imgs/arcos_vallarta_background_hero.png';
+export { default as backgroundPtincipal }          from './bacgroundPrincipal.png';
+export { default as backgroundPtincipalVerde }     from './background-principal-verde.png';
+export { default as backgroundVallartaHero }       from './arcos_vallarta_background_hero.png';
 
-export { default as backgroundPrograma }      from '../imgs/programabackground.png';
-export { default as CAJAAGUA }                from '../imgs/CAJAAGUA.png'; 
-export { default as arcosVallarta }           from '../imgs/arcos-pv-1024x576.jpg';
-export { default as arcosPuertoVallartaHero } from '../imgs/Arcos_puerto_vallarta_hero.png'; 
-export { default as arcosPuertoVallartaSolo } from '../imgs/Arcos-puerto-vallarta-solo.png'; 
-export { default as arcosPuertoVallartaMar } from '../imgs/Arcos_puerto_vallarta_con-mar.png'; 
+export { default as backgroundPrograma }      from './programabackground.png';
+export { default as CAJAAGUA }                from './CAJAAGUA.png'; 
+export { default as arcosVallarta }           from './arcos-pv-1024x576.jpg';
+export { default as arcosPuertoVallartaHero } from './Arcos_puerto_vallarta_hero.png'; 
+export { default as arcosPuertoVallartaSolo } from './Arcos-puerto-vallarta-solo.png'; 
+export { default as arcosPuertoVallartaMar } from './Arcos_puerto_vallarta_con-mar.png'; 
 
 
 // Logos
-import logoReforestamos    from '../imgs/logo RM blanco transparente.png';
-import logoArborDay        from '../imgs/logo arbor day blanco.png';
-import logoPuertoVallarta  from '../imgs/logo_vallarta copia blanco.png';
+import logoReforestamos    from './logo RM blanco transparente.png';
+import logoArborDay        from './logo arbor day blanco.png';
+import logoPuertoVallarta  from './logo_vallarta copia blanco.png';
 
 
 export { logoReforestamos, logoArborDay, logoPuertoVallarta };
-export { default as LogosJuntos       }           from '../imgs/pleca logos.png';
-export { default as LogosJuntosVerde }            from '../imgs/logos-foro.png'  
-export {  default as logoTCOW   }                 from '../imgs/logo tcw blanco.png'
-export {  default as logoReforestamosVerde   }    from '../imgs/LOGO REFORESTAMOS FONDO TRANSPARENTE.png'
+export { default as LogosJuntos       }           from './pleca logos.png';
+export { default as LogosJuntosVerde }            from './logos-foro.png'  
+export {  default as logoTCOW   }                 from './logo tcw blanco.png'
+export {  default as logoReforestamosVerde   }    from './LOGO REFORESTAMOS FONDO TRANSPARENTE.png'
 
 
 // Fotografías perfil
-export { default as fotoErnesto }             from '../imgs/foto-ernesto.jpg';
-export { default as fotoSophie  }             from '../imgs/foto-sophie.png';  
-export { default as fotoIan     }             from '../imgs/Foto-Ian-Hanou.jpg'; 
+export { default as fotoErnesto }             from './foto-ernesto.jpg';
+export { default as fotoSophie  }             from './foto-sophie.png';  
+export { default as fotoIan     }             from './Foto-Ian-Hanou.jpg'; 
 
 
 // objeto logos footer
